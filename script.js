35956abe9a02b693d2307505b7498743388835af
C. Emmanuel Onyema <cemmanuelonyema@gmail.com>
2021-04-19 01:11:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+
+const toggleModal = function() {
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', toggleModal)    
+}
+
+btnCloseModal.addEventListener('click', toggleModal)
+overlay.addEventListener('click', toggleModal)
+
+ document.addEventListener('keydown', function (e) {
+   if (e.key ==='Escape') {
+            console.log(e.key);
+
+       
+   }
+});
+
+
